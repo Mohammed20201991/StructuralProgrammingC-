007fffcbf77957b8aefe6f5b28b7aed90fef59ea
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-03-25 10:41:03 +0300

    Create WhileVsDoWhile.cpp
    while loop did NOT execute because the condition was false from the beginning.
    do...while executed once before checking the condition.
    Task:
    Write a program using while loop to print numbers from 1 to 10.

---
+++ Lab-2/WhileVsDoWhile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+
+    // while loop
+    while (x < 5) {
+        cout << "While Loop\n";
+    }
+
+    // do...while loop
+    do {
+        cout << "Do-While Loop\n";
+    } while (x < 5);
+
+    return 0;
+}
