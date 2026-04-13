1aa215d8bdf586d08e713484504916ccab03083b
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-04-13 23:56:35 +0300

    Implement merging of two arrays into one

--- Lab_5/MergeTwoArrays.cpp
+++ Lab_5/MergeTwoArrays.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int A[3] = {1, 2, 3};
+    int B[3] = {4, 5, 6};
+    int C[6];
+
+    // Copy A
+    for(int i = 0; i < 3; i++)
+        C[i] = A[i];
+
+    // Copy B
+    for(int i = 0; i < 3; i++)
+        C[i + 3] = B[i];
+
+    cout << "Merged Array:\n";
+    for(int i = 0; i < 6; i++)
+        cout << C[i] << " ";
+
+    return 0;
+}
