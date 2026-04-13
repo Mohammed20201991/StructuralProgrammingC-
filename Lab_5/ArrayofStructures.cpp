d7d93fd4a14ef59611b93492028c008637182ab4
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-04-14 00:21:21 +0300

    Add initialization comment for Student structure

--- Lab_5/ArrayofStructures.cpp
+++ Lab_5/ArrayofStructures.cpp
@@ -9,7 +9,8 @@ struct Student {
 
 int main() {
     Student s[3];
-
+// You can initialize directly:
+    // Student s = {"Ali", 101, 90.5};
     for(int i = 0; i < 3; i++) {
         cout << "Enter student " << i+1 << ":\n";
         cin >> s[i].name >> s[i].id >> s[i].grade;
