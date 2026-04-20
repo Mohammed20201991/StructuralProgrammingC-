292d4b9ae4b2d5ebd797fd83ee8f3cb601932bec
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-04-21 00:27:35 +0300

    Add function with return value to calculate sum

---
+++ Lab_7/FunctionwithReturnValue.cpp
@@ -0,0 +1,13 @@
+// Function with Return Value
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int main() {
+  int result = add(5, 3);
+  cout << "Sum = " << result << endl;
+  return 0;
+}
