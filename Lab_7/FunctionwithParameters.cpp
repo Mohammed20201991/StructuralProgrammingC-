7bff41c4a6549cb8544b99c11aeecc7f89d815ea
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-04-21 00:25:43 +0300

    Add FunctionwithParameters.cpp to print names

---
+++ Lab_7/FunctionwithParameters.cpp
@@ -0,0 +1,13 @@
+// Function with Parameters
+#include <iostream>
+using namespace std;
+
+void printName(string name) {
+  cout << "Name: " << name << endl;
+}
+
+int main() {
+  printName("Ali");
+  printName("Sara");
+  return 0;
+}
