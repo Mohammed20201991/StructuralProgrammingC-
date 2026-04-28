1acfc806f637d50752a76a2662e3be4751cb937f
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-04-28 09:24:31 +0300

    Add factorial function using recursion
    Implemented a recursive function to calculate factorial.

--- Lab_8/Recursion.cpp
+++ Lab_8/Recursion.cpp
@@ -1,1 +1,26 @@
-// To Do
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n == 0)  // Base case
+        return 1;
+    else
+        return n * factorial(n - 1);  // Recursive call
+}
+
+int main() {
+    cout << factorial(5);
+    return 0;
+}
+
+
+/*
+factorial(5)
+= 5 * factorial(4)
+= 5 * 4 * factorial(3)
+= 5 * 4 * 3 * factorial(2)
+= 5 * 4 * 3 * 2 * factorial(1)
+= 5 * 4 * 3 * 2 * 1 * factorial(0)
+= 5 * 4 * 3 * 2 * 1 * 1
+= 120
+*/
