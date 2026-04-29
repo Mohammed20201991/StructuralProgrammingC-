fa5923a5df44b206f5245033dc6650ad9ca947fe
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-04-29 11:51:51 +0300

    Update Fibonacci.cpp

--- Lab_8/Fibonacci.cpp
+++ Lab_8/Fibonacci.cpp
@@ -9,12 +9,28 @@ F(n)=F(n−1)+F(n−2)
 /*
 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, .. 
 */
-int fibonacci(int n) {
-    if (n <= 1)  // Base case
-        return n;
-    else
-        return fibonacci(n - 1) + fibonacci(n - 2);
+// int fibonacci(int n) {
+//     if (n <= 1)  // Base case
+//         return n;
+//     else
+//         return fibonacci(n - 1) + fibonacci(n - 2);
+// }
+
+// 1- Iterative Solution 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+    int a = 0, b = 1, next;
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+    return 0;
 }
-
-
 // https://en.wikipedia.org/wiki/Fibonacci_sequence
