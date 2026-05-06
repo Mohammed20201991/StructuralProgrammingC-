8eaee9ab94afb8614c34d47b09219c54968d613e
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-05-06 21:59:37 +0300

    Implement printArray function for pointer usage
    Added a function to print an array using pointers.

--- Lab_10/PointersWithArrays.cpp
+++ Lab_10/PointersWithArrays.cpp
@@ -12,3 +12,19 @@ int main() {
 
     printFirst(data); // works, but hidden pointer behavior
 }
+
+// With Pointers (Clear & Powerful)
+#include <iostream>
+using namespace std;
+
+void printArray(int *p, int size) {
+    for(int i = 0; i < size; i++) {
+        cout << *(p + i) << " ";
+    }
+}
+
+int main() {
+    int data[3] = {10, 20, 30};
+
+    printArray(data, 3); // same as &data[0]
+}
