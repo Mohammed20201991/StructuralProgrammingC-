d1f981a1feca89c8053fde5bfbcfeeeaaff444aa
Al-Hitawi Mohammed <59222637+Mohammed20201991@users.noreply.github.com>
2026-05-06 21:52:30 +0300

    Implement pointer usage in change function
    Demonstrate passing by address using pointers in C++.

--- Lab_10/ExWithPointers.cpp
+++ Lab_10/ExWithPointers.cpp
@@ -1,1 +1,20 @@
+//  With Pointers (Pass by Address)
+#include <iostream>
+using namespace std;
 
+void change(int *x) {
+    *x = 100;   // change the original value
+}
+
+int main() {
+    int a = 10;
+
+    change(&a);
+
+    cout << a;  // Output: 100 (changed)
+}
+/*
+- We passed the address of a
+- Function accessed the real variable using *x
+- So the original value changed
+*/
